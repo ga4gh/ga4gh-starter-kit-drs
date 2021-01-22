453dc5d0a473570bbbf385eaa1221eb9665206c3
Jeremy Adams <jeremy.adams@ga4gh.org>
2021-01-22 14:29:00 -0500

    100% test coverage for models package

--- src/test/java/org/ga4gh/drs/model/ServiceTypeTest.java
+++ src/test/java/org/ga4gh/drs/model/ServiceTypeTest.java
@@ -1,15 +1,40 @@
 package org.ga4gh.drs.model;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ServiceTypeTest {
 
-    @Test
-    public void testServiceType() {
-        new ServiceType();
-        Assert.assertEquals(ServiceType.GROUP, "org.ga4gh");
-        Assert.assertEquals(ServiceType.ARTIFACT, "drs");
-        Assert.assertEquals(ServiceType.VERSION, "1.1.0");
+    @DataProvider(name = "cases")
+    public Object[][] getData() {
+        return new Object[][]{
+            {
+                "org.ga4gh",
+                "drs",
+                "1.1.0"
+            }
+        };
+    }
+
+    public void assertions(ServiceType serviceType, String group, String artifact, String version) {
+        Assert.assertEquals(serviceType.getGroup(), group);
+        Assert.assertEquals(serviceType.getArtifact(), artifact);
+        Assert.assertEquals(serviceType.getVersion(), version);
+    }
+
+    @Test(dataProvider = "cases")
+    public void testServiceTypeNoArgsConstructor(String group, String artifact, String version) {
+        ServiceType serviceType = new ServiceType();
+        serviceType.setGroup(group);
+        serviceType.setArtifact(artifact);
+        serviceType.setVersion(version);
+        assertions(serviceType, group, artifact, version);
+    }
+
+    @Test(dataProvider = "cases")
+    public void testServiceTypeAllArgsConstructor(String group, String artifact, String version) {
+        ServiceType serviceType = new ServiceType(group, artifact, version);
+        assertions(serviceType, group, artifact, version);
     }
 }
