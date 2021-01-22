8be3c1e88d715df42ed4c36d0449c96592c07e57
Anders Leung <anders.leung@ga4gh.org>
2021-01-22 15:55:48 -0500

    Review changes
    * Refactor DataSourceLookup regex handling
    * DataSource.drsIdPattern stored as Pattern and not String
    * DrsObjectLoaderFactory simplified to use switch
    * Missing newline in ResourceNotFoundException

--- src/main/java/org/ga4gh/drs/utils/DataSourceLookup.java
+++ src/main/java/org/ga4gh/drs/utils/DataSourceLookup.java
@@ -1,19 +1,19 @@
 package org.ga4gh.drs.utils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.annotation.PostConstruct;
 import org.ga4gh.drs.AppConfigConstants;
 import org.ga4gh.drs.configuration.DataSource;
 import org.ga4gh.drs.configuration.DrsConfigContainer;
+import org.ga4gh.drs.exception.ResourceNotFoundException;
 import org.ga4gh.drs.model.AccessType;
 import org.ga4gh.drs.utils.objectloader.DrsObjectLoader;
 import org.ga4gh.drs.utils.objectloader.DrsObjectLoaderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DataSourceLookup {
 
@@ -25,82 +25,61 @@ public class DataSourceLookup {
     DrsObjectLoaderFactory drsObjectLoaderFactory;
 
     List<DataSource> dataSources;
-    List<Pattern> patterns;
 
+    private static final Pattern pathTemplateSlotPattern = Pattern.compile("\\{[a-zA-Z][a-zA-Z0-9]*}");
     public DataSourceLookup() {
-        dataSources = new ArrayList<>();
-        patterns = new ArrayList<>();
+
     }
 
     @PostConstruct
     private void postConstruct() {
         dataSources = drsConfigContainer.getDrsConfig().getDataSourceRegistry().getDataSources();
-        for (DataSource dataSource: dataSources) {
-            Pattern pattern = Pattern.compile(dataSource.getDrsIdPattern());
-            patterns.add(pattern);
-        }
     }
 
     public DrsObjectLoader getDrsObjectLoaderFromId(String objectId) {
-        DrsObjectLoader drsObjectLoader = null;
-        int match = findDataSourceMatch(objectId);
-        if (match == -1) {
-            return drsObjectLoader;
+        DataSource source = findDataSourceMatch(objectId);
+
+        if (source == null) {
+            return null;
         }
 
-        String objectPath = renderObjectPath(match, objectId);
-        return newDrsObjectLoader(match, objectPath);
+        String objectPath = renderObjectPath(source, objectId);
+        return newDrsObjectLoader(source, objectPath);
     }
 
-    private int findDataSourceMatch(String objectId) {
-        int match = -1; // -1 indicates no match
-        for (int i = 0; i < dataSources.size(); i++) {
-            Pattern pattern = patterns.get(i);
-            Matcher matcher = pattern.matcher(objectId);
-            if (matcher.find()) {
-                match = i;
-                break;
+    private DataSource findDataSourceMatch(String objectId) {
+        for (DataSource source : dataSources) {
+            if (source.getDrsIdPattern().matcher(objectId).find()) {
+                return source;
             }
         }
-        return match;
+        return null;
     }
 
-    private String renderObjectPath(int match, String objectId) {
-        DataSource dataSource = dataSources.get(match);
-        String drsIdPatternString = dataSource.getDrsIdPattern();
+    @SuppressWarnings("unused")
+    private String renderObjectPath(DataSource source, String objectId) {
+        Pattern drsIdPattern = source.getDrsIdPattern();
 
-        // parse out the regex capture group names (ie. keys)
-        Pattern captureGroupNamePattern = Pattern.compile("\\(\\?<([a-zA-Z][a-zA-Z0-9]*)>");
-        Matcher captureGroupNameMatcher = captureGroupNamePattern.matcher(drsIdPatternString);
-        List<String> captureGroupNames = new ArrayList<>();
-
-        while (captureGroupNameMatcher.find()) {
-            String captureGroupName = captureGroupNameMatcher.group(1);
-            captureGroupNames.add(captureGroupName);
-        }
-
-        // parse out the regex capture group values
-        Pattern drsIdPattern = patterns.get(match);
         Matcher drsIdPatternMatcher = drsIdPattern.matcher(objectId);
+
+        // Find cannot fail because the provided source was selected as its pattern matched
         drsIdPatternMatcher.find();
-        HashMap<String, String> regexMap = new HashMap<>();
-        for (String captureGroupName : captureGroupNames) {
-            String value = drsIdPatternMatcher.group(captureGroupName);
-            regexMap.put(captureGroupName, value);
-        }
-        
+
+        String objectPath = source.getObjectPathTemplate();
+
         // render the object path, ie. by substituting capture group names with specified values
-        String objectPath = dataSource.getObjectPathTemplate();
-        for (String captureGroupName : captureGroupNames) {
-            String toReplace = "{" + captureGroupName + "}";
-            String replacement = regexMap.get(captureGroupName);
-            objectPath = objectPath.replace(toReplace, replacement);
-        }
-        return objectPath;
+        Matcher replacementMatcher = pathTemplateSlotPattern.matcher(objectPath);
+        return replacementMatcher.replaceAll(result -> {
+            // Get name of group with surrounding {}
+            String matched = result.group();
+            // Trim {}
+            String subseq = matched.substring(1, matched.length() - 1);
+            return drsIdPatternMatcher.group(subseq);
+        });
     }
 
-    private DrsObjectLoader newDrsObjectLoader(int match, String objectPath) {
-        AccessType accessType = dataSources.get(match).getProtocol();
+    private DrsObjectLoader newDrsObjectLoader(DataSource source, String objectPath) {
+        AccessType accessType = source.getProtocol();
         DrsObjectLoader drsObjectLoader = drsObjectLoaderFactory.createDrsObjectLoader(accessType);
         drsObjectLoader.setObjectPath(objectPath);
         return drsObjectLoader;
--- src/main/java/org/ga4gh/drs/utils/objectloader/DrsObjectLoaderFactory.java
+++ src/main/java/org/ga4gh/drs/utils/objectloader/DrsObjectLoaderFactory.java
@@ -1,35 +1,22 @@
 package org.ga4gh.drs.utils.objectloader;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-
+import org.ga4gh.drs.exception.ResourceNotFoundException;
 import org.ga4gh.drs.model.AccessType;
 
 public class DrsObjectLoaderFactory {
-
-    private static final HashMap<AccessType,Class<? extends DrsObjectLoader>> objectLoaderDict = loadObjectLoaderDict();
 
     public DrsObjectLoaderFactory() {
 
     }
 
     public DrsObjectLoader createDrsObjectLoader(AccessType accessType) {
-        DrsObjectLoader loader = null;
-
-        try {
-            Class<? extends DrsObjectLoader> loaderClass = objectLoaderDict.get(accessType);
-            loader = loaderClass.getConstructor().newInstance();
-        } catch (NullPointerException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            return loader;
+        switch (accessType) {
+            case FILE:
+                return new FileDrsObjectLoader();
+            case HTTPS:
+                return new HttpsDrsObjectLoader();
+            default:
+                throw new ResourceNotFoundException();
         }
-        return loader;
     }
-
-    private static HashMap<AccessType,Class<? extends DrsObjectLoader>> loadObjectLoaderDict() {
-        return new HashMap<AccessType,Class<? extends DrsObjectLoader>>(){{
-            put(AccessType.FILE, FileDrsObjectLoader.class);
-            put(AccessType.HTTPS, HttpsDrsObjectLoader.class);
-        }};
-    }
-    
 }
