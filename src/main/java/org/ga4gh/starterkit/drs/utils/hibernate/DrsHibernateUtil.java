a9d743b9b9f0b81931656afe8b18602f007e8a52
Jeremy Adams <jeremy.adams@ga4gh.org>
2021-08-17 14:36:41 -0400

    move recursive checksum operations for bundles to separate class

--- src/main/java/org/ga4gh/starterkit/drs/utils/hibernate/DrsHibernateUtil.java
+++ src/main/java/org/ga4gh/starterkit/drs/utils/hibernate/DrsHibernateUtil.java
@@ -11,6 +11,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import org.ga4gh.starterkit.drs.model.Checksum;
 import org.ga4gh.starterkit.drs.model.DrsObject;
+import org.ga4gh.starterkit.drs.utils.BundleRecursiveChecksumCalculator;
 import org.apache.commons.lang3.StringUtils;
 import org.ga4gh.starterkit.common.hibernate.HibernateEntity;
 import org.ga4gh.starterkit.common.hibernate.HibernateUtil;
@@ -47,7 +48,7 @@ public class DrsHibernateUtil extends HibernateUtil {
                 if (recursiveChildLoad) {
                     recursiveDrsObjectChildLoad(drsObject);
                     drsObject.setSize(recursiveSize(drsObject));
-                    drsObject.setChecksums(checksumMapToList(recursiveChecksums(drsObject)));
+                    drsObject.setChecksums(BundleRecursiveChecksumCalculator.getChecksums(drsObject));
                 }
             }
         } catch (PersistenceException e) {
@@ -94,89 +95,6 @@ public class DrsHibernateUtil extends HibernateUtil {
             }
         }
         return sizeSum;
-    }
-
-    private Map<String, String> recursiveChecksums(DrsObject parentDrsObject) {
-        Map<String, String> checksumMap = null;
-        List<DrsObject> childrenDrsObjects = parentDrsObject.getDrsObjectChildren();
-
-        if (childrenDrsObjects != null) {
-            if (childrenDrsObjects.size() == 0) {
-                checksumMap = createChecksumMapFromDrsObjectBlob(parentDrsObject);
-            } else {
-                List<Map<String, String>> childChecksumMaps = new ArrayList<>();
-                for (int i = 0; i < childrenDrsObjects.size(); i++) {
-                    childChecksumMaps.add(recursiveChecksums(childrenDrsObjects.get(i)));
-                }
-                checksumMap = mergeChecksumMaps(childChecksumMaps);
-            }
-        }
-        return checksumMap;
-    }
-
-    private List<Checksum> checksumMapToList(Map<String, String> checksumMap) {
-        List<Checksum> checksumList = new ArrayList<>();
-        for (String key : checksumMap.keySet()) {
-            Checksum checksum = new Checksum();
-            checksum.setType(key);
-            checksum.setChecksum(checksumMap.get(key));
-            checksumList.add(checksum);
-        }
-        return checksumList;
-    }
-
-    private Map<String, String> createChecksumMapFromDrsObjectBlob(DrsObject drsObject) {
-        Map<String, String> checksumMap = new HashMap<>();
-        for (Checksum checksum: drsObject.getChecksums()) {
-            checksumMap.put(checksum.getType(), checksum.getChecksum());
-        }
-        return checksumMap;
-    }
-
-    private Map<String, String> mergeChecksumMaps(List<Map<String, String>> checksumMaps) {
-        // initial setup
-        Map<String, String> mergedChecksumMap = new HashMap<>();
-        Map<String, Integer> checksumTypeCounts = new HashMap<>();
-        int nMaps = checksumMaps.size();
-
-        // first loop, determine the checksum types shared by all children
-        for (Map<String, String> checksumMap : checksumMaps) {
-            for (String key : checksumMap.keySet()) {
-                if (!checksumTypeCounts.containsKey(key)) {
-                    checksumTypeCounts.put(key, 0);
-                }
-                checksumTypeCounts.put(key, checksumTypeCounts.get(key) + 1);
-            }
-        }
-
-        // second loop, for each final checksum type, sort and concatenate values
-        for (String key : checksumTypeCounts.keySet()) {
-            if (checksumTypeCounts.get(key) == nMaps) {
-                List<String> checksumValues = new ArrayList<>();
-                for (Map<String, String> checksumMap : checksumMaps) {
-                    checksumValues.add(checksumMap.get(key));
-                }
-                Collections.sort(checksumValues);
-                String concatenated = StringUtils.join(checksumValues, "");
-
-                String algorithm = null;
-                switch (key) {
-                    case "md5":
-                        algorithm = MessageDigestAlgorithms.MD5;
-                        break;
-                    case "sha1":
-                        algorithm = MessageDigestAlgorithms.SHA_1;
-                        break;
-                    case "sha256":
-                        algorithm = MessageDigestAlgorithms.SHA_256;
-                        break;
-                }
-                String concatDigest = new DigestUtils(algorithm).digestAsHex(concatenated);
-                mergedChecksumMap.put(key, concatDigest);
-            }
-        }
-
-        return mergedChecksumMap;
     }
 
     /**
